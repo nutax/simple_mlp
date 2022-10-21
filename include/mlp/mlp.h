e78031c2c093f40a7f35335e577d4cbe09ff0660
Jose Huby <jose.huby@utec.edu.pe>
2022-10-20 21:01:27 -0500

    Updated core functions declarations and typedef

--- include/mlp/mlp.h
+++ include/mlp/mlp.h
@@ -3,30 +3,36 @@
 
 #include <immintrin.h> // AVX, AVX2, FMA, AVX-512
 #include <stdlib.h> // aligned_alloc, rand, size_t
+#include <assert.h> // assert
 
 
 #define MLP_CACHE_LINE 64
+typedef float float_t;
 
 
 struct mlp;
 struct mlp_layer;
 
 
-void mlp_init(struct mlp *mlp, float const *neurons_per_layer, size_t layers); // Allocates memory, initializes it and set pointers
-float const *mlp_forward(struct mlp *mlp, float const *input); // The input goes into the first hidden layer and that output is the input of the next and so on. Return the pointer to the last layer output
-void mlp_backprog(struct mlp *mlp, float const *answer); // The cached output is compared to the answer and propagates backwards the change in the right direction based on the influence of each neuron
+void mlp_init(struct mlp *mlp, size_t n_vars_per_input, size_t const *n_neurons_per_hidden, size_t n_hiddens, size_t n_vars_per_output); // Allocates memory, initializes it and set pointers
+void mlp_load_input(struct mlp *mlp, float_t const *input); // Memcpys de single input row into aligned address (batch size = 1 for the moment)
+float_t const *mlp_forward(struct mlp *mlp); // The input goes into the first hidden layer and that output is the input of the next and so on. Return the pointer to the last layer output
+void mlp_load_answer(struct mlp *mlp, float_t const *answer); // Memcpys de single answer row into aligned address (batch size = 1 for the moment)
+void mlp_backprog(struct mlp *mlp); // The cached output is compared to the answer and propagates backwards the change in the right direction based on the influence of each neuron
 
 
 struct mlp{
-    int n_layers;
+    size_t n_layers;
     struct mlp_layer *layers;
+    float_t *answer;
+    float_t *aux;
 };
 
 struct mlp_layer{
     size_t n_outputs;
-    float *weights;
-    float *outputs;
-    float *deltas;
+    float_t *weights;
+    float_t *outputs;
+    float_t *deltas;
 };
 
 #endif // _MLP_H_
--- src/mlp/mlp.c
+++ src/mlp/mlp.c
@@ -1,1 +1,27 @@
 #include "mlp/mlp.h"
+
+
+// Allocates memory, initializes it and set pointers
+void mlp_init(struct mlp *mlp, size_t n_vars_per_input, size_t const *n_neurons_per_hidden, size_t n_hiddens, size_t n_vars_per_output){
+
+}
+
+// Memcpys de single input row into aligned address (batch size = 1 for the moment)
+void mlp_load_input(struct mlp *mlp, float_t const *input){
+
+}
+
+// The input goes into the first hidden layer and that output is the input of the next and so on. Return the pointer to the last layer output
+float_t const *mlp_forward(struct mlp *mlp){
+
+}
+
+// Memcpys de single answer row into aligned address (batch size = 1 for the moment)
+void mlp_load_answer(struct mlp *mlp, float_t const *answer){
+
+}
+
+// The cached output is compared to the answer and propagates backwards the change in the right direction based on the influence of each neuron
+void mlp_backprog(struct mlp *mlp){
+
+}
